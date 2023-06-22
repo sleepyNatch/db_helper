ddd1b777b2a9f5aec95c13656570e61e1138b890
sleepyNatch <natchananks@gmail.com>
2023-06-23 00:32:20 +0700

    age sex weight

--- frontend/pages/risk.js
+++ frontend/pages/risk.js
@@ -1,11 +1,21 @@
 'use client'
-import { createContext } from 'react'
-
+import { createContext, useState } from 'react'
 const Context = createContext()
 import * as React from 'react';
-import { FormGroup, FormControlLabel, Checkbox } from '@mui/material';
+import { FormGroup, FormControlLabel, Checkbox, TextField } from '@mui/material';
+
 
 export default function Risk() {
+
+    const [selectedValue, setSelectedValue] = useState('');
+    const [selectedGender, setSelectedGender] = useState('');
+
+    const handleChange = (event) => {
+      setSelectedValue(event.target.value);
+    };
+    const genderChange = (event) => {
+        setSelectedGender(event.target.value);
+      };
 
   return (
     <div>
@@ -17,18 +27,53 @@ export default function Risk() {
         <h1 style={{color:'white',fontSize:30}}> helper </h1>
       </div>
       <br></br> <br></br>
-      <div style={{display:"flex",justifyContent:"center",background:"white"}}>
+      <div style={{display:"flex",justifyContent:"center",flexDirection:"column",background:"white"}}>
         <br></br> <br></br>
         <div style={{display:"flex",justifyContent:"center",background:"#12A596"}}>
           <h1>กรุณาทำเเบบทดสอบความเสี่ยงเพื่อตรวจสอบถึงระดับความเสี่ยงของคุณ</h1>
         </div>
-      </div>
-      <div>
-        <p>1.คุณอายุเท่าไหร่</p>
-        <FormGroup>
-          <FormControlLabel required control={<Checkbox />} label="Required" />
-          <FormControlLabel disabled control={<Checkbox />} label="Disabled" />
+        <div>
+        <p style={{color:'black'}}>1.คุณอายุเท่าไหร่</p>
+        <FormGroup style={{color:'black'}}>
+          <FormControlLabel
+            control={<Checkbox checked={selectedValue === 'option1'} onChange={handleChange} value="option1" />}
+          label="34-39 ปี"
+         />
+        <FormControlLabel
+        control={<Checkbox checked={selectedValue === 'option2'} onChange={handleChange} value="option2" />}
+        label="40-44 ปี"
+      />
+      <FormControlLabel
+        control={<Checkbox checked={selectedValue === 'option3'} onChange={handleChange} value="option3" />}
+        label="45-49 ปี"
+      />
+      <FormControlLabel
+        control={<Checkbox checked={selectedValue === 'option4'} onChange={handleChange} value="option4" />}
+        label="50 ปีขึ้นไป"
+      />
         </FormGroup>
+        <br></br>
+        <p style={{color:'black'}}>2.โปรดระบุเพศของคุณ</p>
+        <FormGroup style={{color:'black'}}>
+          <FormControlLabel
+            control={<Checkbox checked={selectedGender === 'option1'} onChange={genderChange} value="option1" />}
+          label="ชาย"
+         />
+         <FormControlLabel
+            control={<Checkbox checked={selectedGender === 'option2'} onChange={genderChange} value="option2" />}
+        label="หญิง"
+      />
+        </FormGroup>
+        <br></br>
+        <p style={{color:'black'}}>3.โปรดกรอกน้ำหนักของคุณ </p> <br></br>
+        <TextField
+          id="outlined-helperText"
+          label="น้ำหนัก (กิโลกรัม)"
+          defaultValue="กิโลกรัม"
+        />
+
+        <br></br>
+        </div>
       </div>
       </div>
 
