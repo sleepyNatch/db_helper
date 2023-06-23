2aaab5983b6173ea5e85d156150ac21e621b11ea
yingspvd <supavadeeying@outlook.com>
2023-06-24 02:48:50 +0700

    Remove unused code

--- backend/routes/riskRoutes.js
+++ backend/routes/riskRoutes.js
@@ -2,7 +2,7 @@
  * This file is the path that will redirect to the functions in userCon.js
  ******************************************************************************/
 import express from "express";
-import { saveRisk, getRiskResult } from "../controllers/riskCon.js";
+import { saveRisk, getRiskResult } from "../controllers/RiskControl.js";
 const router = express.Router();
 
 router.post("/saveRisk", saveRisk);
